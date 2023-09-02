d102ffa2b2c60fb63432c2204507949b2386526d
vishnu-git888 <143546568+vishnu-git888@users.noreply.github.com>
2023-09-02 12:09:38 +0530

    Add files via upload

---
+++ Arrays/src/array/Example.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int arr[][]= {{10,20,30,40},{50,60,70,80}};
+		System.out.println();
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<=3;j++) {
+				System.out.print(arr[i][j]+" ");
+			}System.out.println("");
+		}
+
+}}
